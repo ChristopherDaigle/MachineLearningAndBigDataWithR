2a9844f934699ffe1acf8609fd8042c75a1e967a
Christopher <daiglechris@iMac.frontierlocal.net>
2018-11-15 13:08:31 -0500

    updates to R

---
+++ Exercises/BoostrappingWithRobustStandardErrors.R
@@ -0,0 +1,139 @@
+#' ___
+#' Chris Daigle
+#' 
+#'
+#' Residual Bootstrapping
+#' 
+#' 
+#' ___
+#' 
+# Exercise
+# 
+# Another bootstrap method in a linear regression model is to
+# resample residuals instead of (X, Y). The former is called Residual
+# Bootstrapping and the latter is called Pairwise Bootstrapping. I beleve that
+# boot() is based on the Pairwise Bootstrap. Estimate the standard error and
+# bootstrap the 95% confidence intervals for the coeficient of horsepower with
+# Residual Bootstrapping.
+library(boot)
+library(ISLR)
+
+set.seed(1)
+
+df <- Auto
+dfBoot <- df[, c(1, 4, 5)]
+
+# Sample Model ####
+X1 <- dfBoot[, 2]
+X2 <- dfBoot[, 3]
+y <- dfBoot[, 1]
+
+lmFit <- lm(y ~ X1 + X2)
+
+beta0Hat <- lmFit$coefficients[1]
+beta1Hat <- lmFit$coefficients[2]
+beta2Hat <- lmFit$coefficients[3]
+
+# t values of sample model ####
+n <- nrow(dfBoot)
+num1 <- sum(((X1 - mean(X1)) ^ 2) * (lmFit$residuals^2))/n
+den1 <- sum((X1-mean(X1))^2)/(n-1)
+robustSE1 <- sqrt(num1)/den1
+
+num2 <- sum(((X2 - mean(X2)) ^ 2) * (lmFit$residuals^2))/n
+den2 <- sum((X2-mean(X2))^2)/(n-1)
+robustSE2 <- sqrt(num2)/den2
+
+robT1Coef <- sqrt(n) * beta1Hat / robustSE1
+robT2Coef <- sqrt(n) * beta2Hat / robustSE2
+
+t1Coef <- summary(lmFit)$coefficients[2,3]
+t2Coef <- summary(lmFit)$coefficients[3,3]
+
+# Residual Bootstrap Model
+# Initialize relevant values ####
+R <- 1000
+
+robustT1Star <- NA
+robustT2Star <- NA
+
+t1Star <- NA
+t2Star <- NA
+
+# Loop for residual bootstrapping with and without robust standard errors ####
+for (r in 1:R) {
+  uStar <- sample(lmFit$residual, nrow(dfBoot), replace = TRUE)
+  
+  yStar <- beta0Hat + beta1Hat * X1 + beta2Hat * X2 + uStar
+  
+  lmFitStar <- lm(yStar ~ X1 + X2)
+  beta1Star <- lmFitStar$coefficients[2]
+  beta2Star <- lmFitStar$coefficients[3]
+  seStar1 <- summary(lmFitStar)$coefficients[2,2]
+  seStar2 <- summary(lmFitStar)$coefficients[3,2]
+  
+  num1Star <- sum(((X1 - mean(X1)) ^ 2) * (lmFitStar$residuals^2))/n
+  den1Star <- sum((X1-mean(X1))^2)/(n-1)
+  robustSE1Star <- sqrt(num1Star)/den1Star
+  
+  num2Star <- sum(((X2 - mean(X2)) ^ 2) * (lmFitStar$residuals^2))/n
+  den2Star <- sum((X2-mean(X2))^2)/(n-1)
+  robustSE2Star <- sqrt(num2Star)/den2Star
+  
+  robustT1Star[r] <- sqrt(n) * (beta1Star - beta1Hat) / robustSE1Star
+  robustT2Star[r] <- sqrt(n) * (beta2Star - beta2Hat) / robustSE2Star
+  
+  t1Star[r] <- (beta1Star - beta1Hat) / seStar1
+  t2Star[r] <- (beta2Star - beta2Hat) / seStar2
+}
+
+# Sort the residual moel t values ####
+robustT1Star <- sort(robustT1Star)
+robustT2Star <- sort(robustT2Star)
+
+t1Star <- sort(t1Star)
+t2Star <- sort(t2Star)
+
+# Get the 2.5% and 97.5% levels ####
+critT1Star <- c(t1Star[25], t1Star[975])
+critT2Star <- c(t2Star[25], t2Star[975])
+
+critRobustT1Star <- c(robustT1Star[25], robustT1Star[975])
+critRobustT2Star <- c(robustT2Star[25], robustT2Star[975])
+
+# Plot and cut the values ####
+hist(t1Star, breaks = 30, probability = TRUE, col= "grey", 
+     main = "Distribution of Horsepower's t* Under Residual Bootstrap")
+lines(density(t1Star), col = "red", lwd = 3)
+abline(v = c(critT1Star, beta1Hat), col = c("blue"), lty = c(2, 2, 1), lwd = 3)
+cat(paste("The coefficient of intrest is", round(beta1Hat, 3), 
+          "and the t-statistic is", round(t1Coef, 3), "."))
+cat(paste("The 95% critical values are", round(critT1Star[1], 3), 
+          "and", round(critT1Star[2], 3), "."))
+
+hist(t2Star, breaks = 30, probability = TRUE, col= "grey", 
+     main = "Distribution of Weight's t* Under Residual Bootstrap")
+lines(density(t2Star), col = "red", lwd = 3)
+abline(v = c(critT2Star, beta2Hat), col = c("blue"), lty = c(2, 2, 1), lwd = 3)
+cat(paste("The coefficient of intrest is", round(beta2Hat, 3), 
+          "and the t-statistic is", round(t2Coef, 3), "."))
+cat(paste("The 95% critical values are", round(critT2Star[1], 3), 
+          "and", round(critT2Star[2], 3), "."))
+
+hist(robustT1Star, breaks = 30, probability = TRUE, col= "grey", 
+     main = "Distribution of Horsepower's robust t* Under Residual Bootstrap")
+lines(density(robustT1Star), col = "red", lwd = 3)
+abline(v = c(critRobustT1Star, beta1Hat), col = c("blue"), lty = c(2, 2, 1), lwd = 3)
+cat(paste("The coefficient of intrest is", round(beta1Hat, 3), 
+          "and the t-statistic is", round(robT1Coef, 3), "."))
+cat(paste("The 95% critical values are", round(critRobustT1Star[1], 3), 
+          "and", round(critRobustT1Star[2], 3), "."))
+
+hist(robustT2Star, breaks = 30, probability = TRUE, col= "grey", 
+     main = "Distribution of Weight's robust t* Under Residual Bootstrap")
+lines(density(robustT2Star), col = "red", lwd = 3)
+abline(v = c(critRobustT2Star, beta2Hat), col = c("blue"), lty = c(2, 2, 1), lwd = 3)
+cat(paste("The coefficient of intrest is", round(beta2Hat, 3), 
+          "and the t-statistic is", round(robT2Coef, 3), "."))
+cat(paste("The 95% critical values are", round(critRobustT2Star[1], 3), 
+          "and", round(critRobustT2Star[2], 3), "."))
---
+++ Exercises/CrossValidation.R
@@ -0,0 +1,112 @@
+#' ---
+#' title: "Homework 6"
+#' author: "Christopher Daigle"
+#' date: "Oct. 23 2018"
+#' output: pdf_document
+#' ---
+library(boot)
+library(ISLR)
+rm(list = ls())
+set.seed(1)
+
+#' Exercise 1 ####
+#' Cross validation can also be used to estimate the test error for a
+#' classification problem. Run a logit model with the Smarket data. The dependent
+#' variable is Direction and
+
+glm.fit <-
+  glm(Direction ~ Lag1 + Lag2, family = binomial, data = Smarket)
+summary(glm.fit)
+
+#' Compare this model with the following models using K-Fold cross-validation
+#' with K=10 Direction ~Lag1+Lag2+Lag3, Direction ~Lag1+Lag2+Lag3+Lag4, Direction
+#' ~Lag1+Lag2+Lag3+Lag5
+
+# Creating K-Fold 'bins'
+n <- nrow(Smarket)
+x <- 1:n
+cv.error <- matrix(NA, 4, 11)
+rownames(cv.error) <- c('Model1', 'Model2', 'Model3', 'Model4')
+colnames(cv.error) <-
+  c(
+    'MSEK-Fold1',
+    'MSEK-Fold2',
+    'MSEK-Fold3',
+    'MSEK-Fold4',
+    'MSEK-Fold5',
+    'MSEK-Fold6',
+    'MSEK-Fold7',
+    'MSEK-Fold8',
+    'MSEK-Fold9',
+    'MSEK-Fold10',
+    'MeanMSE'
+  )
+
+for (i in 1:4) {
+  for (j in 1:10) {
+    glm.fit1 <-
+      glm(Direction ~ Lag1 + Lag2, family = binomial, data = Smarket)
+    glm.fit2  <-
+      glm(Direction ~ Lag1 + Lag2 + Lag3,
+          family = binomial,
+          data = Smarket)
+    glm.fit3  <-
+      glm(Direction ~ Lag1 + Lag2 + Lag3 + Lag4,
+          family = binomial,
+          data = Smarket)
+    glm.fit4  <-
+      glm(Direction ~ Lag1 + Lag2 + Lag3 + Lag4 + Lag5,
+          family = binomial,
+          data = Smarket)
+    cv.error[i, j] <- cv.glm(Smarket, glm.fit1, K = 10)$delta[1]
+  }
+  cv.error[i, 11] <- mean(cv.error[i,], na.rm = TRUE)
+}
+cv.error
+
+numbers = c('first', 'second', 'third', 'fourth')
+for (i in 1:4) {
+  print(paste('The MSE for the', numbers[i], 'model is:', cv.error[i,11]))
+}
+
+#' Exercise 2 ####
+#' Consider KNN Estimation to predict direction using Lag1 and Lag2. To choose
+#' the optimal number of neighbors, use the K=10. Fold cross validation. Use only
+#' 2004 and 2005 year data. Instructed to use all of the data set for divisibility.
+library(ISLR)
+library(class)
+rm(list = ls())
+set.seed(1)
+
+df <- Smarket
+X <- df[, c('Lag1', 'Lag2')]
+y <- df$Direction
+
+n <- nrow(df)
+ind <- 1:n
+
+optKs <- rep(NA,10)
+
+for (i in 1:10) {
+  testSplit <- sample(ind, size = n/10, replace = FALSE)
+  
+  train <- df[-testSplit, ]
+  test <- df[testSplit, ]
+  
+  trainX <- train[, c('Lag1', 'Lag2')]
+  testX <- test[, c('Lag1', 'Lag2')]
+  trainY <- train$Direction
+  testY <- test$Direction
+  
+  ind <- ind[-testSplit]
+  
+  Acc <- rep(NA, 100)
+  
+  for (j in 1:100) {
+    knnPred <- knn(trainX, testX, trainY, k = j)
+    Acc[j] <- mean(testY == knnPred)
+  }
+  optKs <- which.max(Acc)
+}
+optKs
+print(paste('The best K for this is', optKs))
