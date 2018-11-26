dde6269a7106a3651c923ecf95b6cab97f2fbb6c
Chris Daigle <chris.daigle@uconn.edu>
2018-11-26 14:09:33 -0500

    update to assembly and project

--- Project/DaiglePredictionofSocialSecurity.R
+++ Project/DaiglePredictionofSocialSecurity.R
@@ -13,3 +13,42 @@ df <- read.csv(dataName, stringsAsFactors = FALSE)
 head(df)
 df <- df[-1]
 head(df)
+str(df)
+
+# Variable Manipulation ####
+# Set dates
+df$date <- as.Date(df$date, "%Y-%m-%d")
+# Functions to clean data
+spaceless <- function(x) {
+  x <- gsub(" ", ".", x)
+  x
+}
+
+commaless <- function(x) {
+  x <- gsub(",", "", x)
+  x
+}
+
+dollarless <- function(x) {
+  x <- gsub("\\$", "", x)
+  x
+}
+
+#loops to apply functions
+for (i in 15:20) {
+  df[,i] <- commaless(df[,i])
+}
+for (i in 15:20) {
+  df[,i] <- dollarless(df[,i])
+}
+
+#loop to transform variable types
+for (i in 15:20) {
+  df[,i] <- as.numeric(df[,i])
+}
+# Check that the transformations applied
+str(df)
+
+# Variable Creation ####
+latestDate <- tail(df$date, n=1)
+df$inflator <- df[df$date == latestDate]$cpi / df$cpi
