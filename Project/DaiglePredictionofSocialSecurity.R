fe8cd695143953b6e620f0bc3efa3fbd31b32d69
Chris Daigle <chris.daigle@uconn.edu>
2018-12-04 00:19:36 -0500

    updates through hypothesis testing for stationarity, ready for models

--- Project/DaiglePredictionofSocialSecurity.R
+++ Project/DaiglePredictionofSocialSecurity.R
@@ -6,19 +6,21 @@
 
 # Prepare workspace ####
 rm(list = ls())
+library(tseries)
+library(quantmod)
 setwd('~/Git/MachineLearningAndBigDataWithR/Data')
 dataName <- 'assembled.csv'
 df <- read.csv(dataName, stringsAsFactors = FALSE)
 # Summarize and clean data ####
-head(df)
+# head(df)
 df <- df[-1]
-head(df)
-str(df)
+# head(df)
+# str(df)
 
 # Variable Manipulation ####
 # Set dates
 df$date <- as.Date(df$date, "%Y-%m-%d")
-# Functions to clean data
+# Functions to clean data #
 spaceless <- function(x) {
   x <- gsub(" ", ".", x)
   x
@@ -34,8 +36,7 @@ dollarless <- function(x) {
   x
 }
 
-
-#loops to apply functions
+# Loops to apply functions #
 for (i in 15:20) {
   df[, i] <- commaless(df[, i])
 }
@@ -43,7 +44,7 @@ for (i in 15:20) {
   df[, i] <- dollarless(df[, i])
 }
 
-#loop to transform variable types
+# Loop to transform variable types #
 for (i in 15:20) {
   df[, i] <- as.numeric(df[, i])
 }
@@ -70,7 +71,7 @@ for (i in 15:20) {
 # 20 averageFemaleSSRetiredPay: num
 # 21 cpi                      : num
 #
-# Names with Index ####
+# Order Change #
 df <- df[, c(1, 15, 17, 19, 21, 14, 7, 13, 2:6, 8:12, 16, 18, 20)]
 # 1 date                     : Date
 # 2 totalSSRetired           : num
@@ -110,11 +111,12 @@ realNames <-
 
 df[, realNames] <- df$inflator * df[10:22]
 
-# Differences
+# Differences #
 diffNames <-
   paste('diff',
         c(colnames(df[10:22]),
-          paste('Real', colnames(df[10:22]),
+          paste('Real',
+                colnames(df[10:22]),
                 sep = "")),
         sep = "")
 df[, diffNames] <- rep(NA, nrow(df))
@@ -130,14 +132,17 @@ for (i in 62:64) {
   df[, i][2:nrow(df)] <- diff(df[, i - 60], lag = 1)
 }
 
-# Positive Indicator
-posNames <- paste('pos',
-                  c(colnames(df[10:22]),
-                    paste('Real', colnames(df[10:22]), sep = "")),
-                  sep = "")
+# Positive Indicator #
+posNames <-
+  paste('pos',
+        c(colnames(df[10:22]),
+          paste('Real',
+                colnames(df[10:22]),
+                sep = "")),
+        sep = "")
 df[, posNames] <- rep(0, nrow(df))
 for (i in 65:90) {
-  df[, i][df[, i - 29] > 0] <- 1
+  df[, i][df[, i - 20] > 0] <- 1
 }
 
 posTargetNames <-
@@ -149,39 +154,209 @@ for (i in 91:93) {
   df[, i][df[, i - 29] > 0] <- 1
 }
 
-df <- df[, c(1:9, 62:64, 91:93, 10:61, 65:90)]
-df <- df[complete.cases(df),]
-# Visulizations ####
+# Percent Changes #
+percChangeNames <-
+  paste('percChange',
+        c(colnames(df[10:22]),
+          paste('Real', colnames(df[10:22]), sep = "")),
+        sep = "")
+df[, percChangeNames] <- rep(NA, nrow(df))
+
+for (i in 94:119) {
+  df[, i] <- Delt(df[, i - 84])
+}
+for (i in 94:119) {
+  df[, i] <- as.numeric(df[, i])
+}
+percChangeTargetNames <-
+  paste('percChange',
+        c(colnames(df[2:4])),
+        sep = "")
+df[, percChangeTargetNames] <- rep(NA, nrow(df))
+for (i in 120:122) {
+  df[, i] <- Delt(df[, i - 118])
+}
+for (i in 120:122) {
+  df[, i] <- as.numeric(df[, i])
+}
+
+# Place all target variables - totalRetired* - together
+df <- df[, c(1:4, 62:64, 91:93, 120:122, 5:61, 65:90, 94:119)]
+df1 <- df[complete.cases(df), ]
+
+# Timeseries Evaluation ####
+realDJIOpen <-
+  ts(
+    df$realDJIopen,
+    start = c(1985, 1),
+    end = c(2018, 9),
+    frequency = 12
+  )
+percRealDJIOpen <-
+  ts(
+    df1$percChangeRealDJIopen,
+    start = c(1985, 2),
+    end = c(2018, 9),
+    frequency = 12
+  )
+realSPOpen <-
+  ts(
+    df$realSPopen,
+    start = c(1985, 1),
+    end = c(2018, 9),
+    frequency = 12
+  )
+percRealSPOpen <-
+  ts(
+    df1$percChangeRealSPopen,
+    start = c(1985, 2),
+    end = c(2018, 9),
+    frequency = 12
+  )
+fedFund <-
+  ts(
+    df$fedFundRate,
+    start = c(1985, 1),
+    end = c(2018, 9),
+    frequency = 12
+  )
+
+
+plot(stl(realDJIOpen, s.window = "period"), lwd = 1)
+title(main = 'Seasonal Decomp of Real DJI Open (Sep 2018 Dollars)')
+
+plot(stl(log(realDJIOpen), s.window = "period"), lwd = 1)
+title(main = 'Seasonal Decomp of the Log of Real DJI Open (Sep 2018 Dollars)')
+
+plot(stl(percRealDJIOpen, s.window = "period"), lwd = 1)
+title(main = 'Seasonal Decomp of the Percent Change of Real DJI Open (Sep 2018 Dollars)')
+
+plot(realDJIOpen,
+     col = 'blue',
+     lwd = 3,
+     ylab = 'Dollars USD')
+abline(reg = lm(realDJIOpen ~ time(realDJIOpen)), lwd = 3)
+title(main = 'Real DJI Open (Sep 2018 Dollars)')
+
+plot(log(realDJIOpen),
+     col = 'blue',
+     lwd = 3,
+     ylab = 'Dollars USD')
+abline(reg = lm(log(realDJIOpen) ~ time(log(realDJIOpen))), lwd = 3)
+title(main = 'Log of Real DJI Open (Sep 2018 Dollars)')
+
+plot(percRealDJIOpen,
+     col = 'blue',
+     lwd = 3,
+     ylab = 'Dollars USD')
+abline(reg = lm(percRealDJIOpen ~ time(percRealDJIOpen)), lwd = 3)
+title(main = 'Percent Change of Real DJI Open (Sep 2018 USD)')
+
+plot(stl(realSPOpen, s.window = "period"), lwd = 1)
+title(main = 'Seasonal Decomp of Real SP Open (Sep 2018 Dollars)')
+
+plot(stl(log(realSPOpen), s.window = "period"), lwd = 1)
+title(main = 'Seasonal Decomp of the Log of Real SP Open (Sep 2018 Dollars)')
+
+plot(stl(percRealSPOpen, s.window = "period"), lwd = 1)
+title(main = 'Seasonal Decomp of the Percent Change of Real SP Open (Sep 2018 Dollars)')
+
+plot(realSPOpen,
+     col = 'blue',
+     lwd = 3,
+     ylab = 'Dollars USD')
+abline(reg = lm(realSPOpen ~ time(realSPOpen)), lwd = 3)
+title(main = 'Real SP Open (Sep 2018 Dollars)')
+
+plot(log(realSPOpen),
+     col = 'blue',
+     lwd = 3,
+     ylab = 'Dollars USD')
+abline(reg = lm(log(realSPOpen) ~ time(log(realSPOpen))), lwd = 3)
+title(main = 'Log of Real SP Open (Sep 2018 Dollars)')
+
+plot(percRealSPOpen,
+     col = 'blue',
+     lwd = 3,
+     ylab = 'Dollars USD')
+abline(reg = lm(percRealSPOpen ~ time(percRealSPOpen)), lwd = 3)
+title(main = 'Percent Change of Real SP Open (Sep 2018 USD)')
+
+plot(stl(fedFund, s.window = "period"), lwd = 1)
+title(main = 'Seasonal Decomp of Federal Funds Rate')
+
+plot(fedFund,
+     col = 'blue',
+     lwd = 3,
+     ylab = 'Percent')
+abline(reg = lm(fedFund ~ time(fedFund)), lwd = 3)
+title(main = 'Federal Funds Rate')
+
+plot(log(fedFund),
+     col = 'blue',
+     lwd = 3,
+     ylab = 'Percent (%)')
+abline(reg = lm(log(fedFund) ~ time(log(fedFund))), lwd = 3)
+title(main = 'Log of Federal Funds Rate')
+
+# Hypothesis Tests ####
+adf.test(realDJIOpen, alternative = 'stationary')
+# Augmented Dickey-Fuller Test
+# Fail to reject the null (0.8076 = p > 0.05), so it's likely non-stationary
+adf.test(log(realDJIOpen), alternative = 'stationary')
+# Fail to reject the null (0.4473 = p > 0.05), so it's likely non-stationary still with the transform
+adf.test(percRealDJIOpen, alternative = 'stationary')
+# Reject the null (0.01 = p < 0.05), so it's likely stationary still with the transform
+#' Will use percRealDJIOpen as a predictor
+
+adf.test(realSPOpen, alternative = 'stationary')
+# Augmented Dickey-Fuller Test
+# Fail to reject the null (0.783 = p > 0.05), so it's likely non-stationary
+adf.test(log(realSPOpen), alternative = 'stationary')
+# Fail to reject the null (0.5234 = p > 0.05), so it's likely non-stationary still with the transform
+adf.test(percRealSPOpen, alternative = 'stationary')
+# Reject the null (0.01 = p < 0.05), so it's likely stationary still with the transform
+#' Will use percRealSPOpen as a predictor
+
+adf.test(fedFund, alternative = 'stationary')
+# Fail to reject the null (0.05242 = p > 0.05), so it's on the cusp of being likely non-stationary
+#' Will try model with fedFund as a predictor
+adf.test(log(fedFund), alternative = 'stationary')
+# Fail to reject the null (0.6728 = p > 0.05), so it's likely non-stationary with the transform
+
+# Remove nominal values aside indicators of positive change
+df2 <- df1[, c(1:18, 32:44, 58:96, 110:122)]
+# Visualizations ####
 plot(
-  x = df$date,
-  y = df$diffRealDJIclose,
+  x = df2$date,
+  y = df2$percChangeRealDJIopen,
   col = 'blue',
   lwd = 1,
   type = 'l',
-  ylab = 'US Dollars ($)',
+  ylab = 'USD ($)',
   xlab = 'Date'
 )
 points(
-  x = df$date[df$postotalSSRetired == 1],
-  y = df$diffRealDJIclose[df$postotalSSRetired == 1],
+  x = df2$date[df$postotalSSRetired == 1],
+  y = df2$percChangeRealDJIopen[df2$postotalSSRetired == 1],
   pch = 24,
   col = 'darkgreen',
   cex = 0.8,
   lwd = 3
 )
 points(
-  x = df$date[df$postotalSSRetired == 0],
-  y = df$diffRealDJIclose[df$postotalSSRetired == 0],
+  x = df2$date[df2$postotalSSRetired == 0],
+  y = df2$percChangeRealDJIopen[df2$postotalSSRetired == 0],
   pch = 25,
   col = 'darkred',
   cex = 0.8,
   lwd = 3
 )
 legend(
-  'topleft',
+  'bottomright',
   legend = c(
-    'Monthly Change in DJI Close',
-    c('Positive ∆ in SS', 'Negative ∆ in SS')
+    'Monthly Percent Change in DJI Open',
+    c('Pos. Change in SS', 'Neg. Change in SS')
   ),
   lty = c(1, c(NA, NA)),
   pch = c(NA, c(24, 25)),
@@ -189,31 +364,82 @@ legend(
   bg = c(NA, c('darkgreen', 'darkred')),
   lwd = c(2, c(3, 3))
 )
-title(main = 'Change in DJI Close')
+title(main = 'Monthly % Change in Real DJI Open (Sep 2018 USD)')
 
-# Timeseries Evaluation ####
-library(tseries)
-diffDJIOpen <-
-  ts(
-    df$diffDJIOpen,
-    start = c(1985, 1),
-    end = c(2018, 9),
-    frequency = 12)
+plot(
+  x = df2$date,
+  y = df2$percChangeRealSPopen,
+  col = 'blue',
+  lwd = 1,
+  type = 'l',
+  ylab = 'USD ($)',
+  xlab = 'Date'
+)
+points(
+  x = df2$date[df$postotalSSRetired == 1],
+  y = df2$percChangeRealSPopen[df2$postotalSSRetired == 1],
+  pch = 24,
+  col = 'darkgreen',
+  cex = 0.8,
+  lwd = 3
+)
+points(
+  x = df2$date[df2$postotalSSRetired == 0],
+  y = df2$percChangeRealSPopen[df2$postotalSSRetired == 0],
+  pch = 25,
+  col = 'darkred',
+  cex = 0.8,
+  lwd = 3
+)
+legend(
+  'bottomright',
+  legend = c(
+    'Monthly Percent Change in SP Open',
+    c('Pos. Change in SS', 'Neg. Change in SS')
+  ),
+  lty = c(1, c(NA, NA)),
+  pch = c(NA, c(24, 25)),
+  col = c('blue', c('darkgreen', 'darkred')),
+  bg = c(NA, c('darkgreen', 'darkred')),
+  lwd = c(2, c(3, 3))
+)
+title(main = 'Monthly % Change in Real S&P500 Open (Sep 2018 USD)')
 
-diffTotalSS <-
-  ts(
-    df$diffTotalSSRetired,
-    start = c(1985, 1),
-    end = c(2018, 9),
-    frequency = 12)
-PosRet <-
-  ts(
-    df$totalSSRetiredPos,
-    start = c(1985, 1),
-    end = c(2018, 9),
-    frequency = 12)
+plot(
+  x = df2$date,
+  y = df2$fedFundRate,
+  col = 'blue',
+  lwd = 1,
+  type = 'l',
+  ylab = 'Interest Rate (%)',
+  xlab = 'Date'
+)
+points(
+  x = df2$date[df$postotalSSRetired == 1],
+  y = df2$fedFundRate[df2$postotalSSRetired == 1],
+  pch = 24,
+  col = 'darkgreen',
+  cex = 0.8,
+  lwd = 3
+)
+points(
+  x = df2$date[df2$postotalSSRetired == 0],
+  y = df2$fedFundRate[df2$postotalSSRetired == 0],
+  pch = 25,
+  col = 'darkred',
+  cex = 0.8,
+  lwd = 3
+)
+legend(
+  'topright',
+  legend = c('Federal Funds Rate',
+             c('Pos. Change in SS', 'Neg. Change in SS')),
+  lty = c(1, c(NA, NA)),
+  pch = c(NA, c(24, 25)),
+  col = c('blue', c('darkgreen', 'darkred')),
+  bg = c(NA, c('darkgreen', 'darkred')),
+  lwd = c(2, c(3, 3))
+)
+title(main = 'Federal Funds Rate')
 
-
-plot(stl(DJIOpen, s.window = "period"), lwd = 1)
-plot(stl(log(DJIOpen), s.window = "period"), lwd = 1)
-stl(DJIOpen, s.window = "period")
+# Model ####
